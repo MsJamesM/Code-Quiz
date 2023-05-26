a386eb4cb1c97a0aef751156ed246f59a6ba2cab
MsJamesM <118479305+MsJamesM@users.noreply.github.com>
2023-05-26 07:46:18 -0600

    added simple local storage feature to save user score

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ var questions1 = $("questions1");
 var questions2 = $("questions2");
 var results = $("results");
 var result = $("#result");
-var savedScore = $("#savedScore");
 var counter = 0;
 
-// simple counter keeps track of correct answers
+// -------------------- simple counter keeps track of correct answers
+
+var counter = localStorage.getItem("lastScore");
+lastScore.textContent = counter;
+
 var counter = 0;
 function correctCounter() {
   counter += 1;
   document.getElementById("result").innerHTML = counter;
-  localStorage.setItem("counter", counter);
+  document.getElementById("lastScore").innerHTML = counter;
+  localStorage.setItem("lastScore", counter);
 }
