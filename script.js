905c851f26c0407433f4ba2f805183bc14abd3eb
MsJamesM <118479305+MsJamesM@users.noreply.github.com>
2023-06-22 17:35:07 -0600

    starting fresh four weeks later

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
-var a1 = $("a1");
-var a2 = $("a2");
-var questions1 = $("questions1");
-var questions2 = $("questions2");
-var results = $("results");
-var result = $("#result");
-var counter = 0;
+$(document).ready(function () {
+  var currentQuestion = 0;
+  var score = 0;
+  const questions = [
+    {
+      question: "Question #1",
+      choices: ["answer #1", "answer #2", "answer #3"],
+      correctAnswer: 1,
+    },
+    {
+      question: "Question #2",
+      choices: ["answer #1", "answer #2", "answer #3"],
+      correctAnswer: 3,
+    },
+  ];
 
-// -------------------- simple counter keeps track of correct answers
+  // displays questions
 
-var counter = localStorage.getItem("lastScore");
-lastScore.textContent = counter;
+  function showQuestion() {
+    let question = questions[currentQuestion];
+    $("#question").text(question.question);
 
-var counter = 0;
-function correctCounter() {
-  counter += 1;
-  document.getElementById("result").innerHTML = counter;
-  document.getElementById("lastScore").innerHTML = counter;
-  localStorage.setItem("lastScore", counter);
-}
+    let choices = question.choices;
+    $("#choices").empty();
+    for (let i = 0; i < choices.length; i++) {
+      $("#choices").append(
+        "<li><input type='radio' name='choice' value='" +
+          i +
+          "'>" +
+          choices[i] +
+          "</li>"
+      );
+    }
+  }
 
-function clearAll() {
-  window.localStorage.clear();
-  window.location.reload();
-}
+  // checks correct answers
+
+  function checkAnswer() {
+    let selectedChoice = $("input[name='choice']:checked").val();
+    let question = questions[currentQuestion];
+    if (parseInt(selectedChoice) === question.correctAnswer) {
+      score++;
+    }
+
+    currentQuestion++;
+    if (currentQuestion >= questions.length) {
+      showResult();
+    } else {
+      showQuestion();
+    }
+  }
+
+  // displays final results
+
+  function showResult() {
+    $("#quizContainer").hide();
+    $("#result").text("Your score: " + score + " out of " + questions.length);
+    $("#result").show();
+  }
+
+  function startQuiz() {
+    $("#startButton").hide();
+    $("#quizContainer").show();
+    showQuestion();
+    $("#submit").click(checkAnswer);
+  }
+
+  $("#startButton").click(startQuiz);
+});
